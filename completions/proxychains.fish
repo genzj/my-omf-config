4ab2e0d0040a4b8ce53fec25d5f6ea76b831a4f9
genzj <zj0512@gmail.com>
2018-04-14 16:00:40 +0800

    add completion folder and proxychains subcommand completion

---
+++ completions/proxychains.fish
@@ -0,0 +1,2 @@
+complete -c proxychains -d "Command to run" -x -a "(__fish_complete_subcommand)"
+
--- init.fish
+++ init.fish
@@ -1,3 +1,7 @@
+set -l addtional_complete $OMF_CONFIG/completions
+contains $addtional_complete $fish_complete_path ; \
+    or set -g fish_complete_path $addtional_complete $fish_complete_path
+
 for x in $OMF_CONFIG/conf.d/*.fish
     source $x
 end

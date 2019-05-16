07c3690ea7950febdde76aa20a681f1b6fa3fea0
genzj <zj0512@gmail.com>
2019-05-16 20:13:47 +0800

    add apt related abbr

--- conf.d/09-abbr.fish
+++ conf.d/09-abbr.fish
@@ -3,5 +3,7 @@ if status --is-interactive
     abbr --add --global p 'proxychains'
     abbr --add --global s 'sudo'
     abbr --add --global sp 'sudo proxychains'
-    # etcetera
+    abbr --add --global aptupd 'sudo proxychains apt update'
+    abbr --add --global aptupg 'sudo proxychains apt upgrade'
+    abbr --add --global aptupl 'sudo proxychains apt list --upgradable'
 end
